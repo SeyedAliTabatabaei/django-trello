7e63c3c2e09b95803e324f2d760416f223d74bd5
sinatb <seyedalitabatabaeishere@gmail.com>
2025-03-17 19:55:24 +0330

    logout button added

--- Trello/user/urls.py
+++ Trello/user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from .views import *
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_view, name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
